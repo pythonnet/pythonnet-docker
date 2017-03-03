4282b95f902935c649bac766aed27a1b445918d5
Victor Uriarte <victor.m.uriarte@intel.com>
2017-03-02 19:10:27 -0700

    Update account name

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 
 *Just add python sources and enjoy!*
 
-[https://hub.docker.com/r/pythondotnet/pythonnet/](https://hub.docker.com/r/pythondotnet/pythonnet/)
+[https://hub.docker.com/r/pythonnet/pythonnet/](https://hub.docker.com/r/pythonnet/pythonnet/)
 
 ## Current Support
 
 - Python 2.7 & 3.5
 - Mono 4.8.0
-- Pythonnet 2.2.0
+- Pythonnet 2.2.2
 
 *Note: The dockerfiles are named with their respective combinations.*
--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-docker build -t pythondotnet/pythonnet:python2.7-mono4.8.0-pythonnet2.2.2 -f Dockerfile-python2.7-mono4.8.0-pythonnet2.2.2 .
-docker build -t pythondotnet/pythonnet:python3.5-mono4.8.0-pythonnet2.2.2 -f Dockerfile-python3.5-mono4.8.0-pythonnet2.2.2 .
+docker build -t pythonnet/pythonnet:python2.7-mono4.8.0-pythonnet2.2.2 -f Dockerfile-python2.7-mono4.8.0-pythonnet2.2.2 .
+docker build -t pythonnet/pythonnet:python3.5-mono4.8.0-pythonnet2.2.2 -f Dockerfile-python3.5-mono4.8.0-pythonnet2.2.2 .
 
-docker push pythondotnet/pythonnet:python2.7-mono4.8.0-pythonnet2.2.2
-docker push pythondotnet/pythonnet:python3.5-mono4.8.0-pythonnet2.2.2
+docker push pythonnet/pythonnet:python2.7-mono4.8.0-pythonnet2.2.2
+docker push pythonnet/pythonnet:python3.5-mono4.8.0-pythonnet2.2.2
