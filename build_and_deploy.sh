256b0cf50aac953f1d804c1509cba218911a1730
denfromufa <denfromufa@users.noreply.github.com>
2017-03-12 21:30:40 -0500

    Update build_and_deploy.sh

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-docker build -t pythonnet/pythonnet:python2.7-mono4.8.0-pythonnet2.2.2 -f Dockerfile-python2.7-mono4.8.0-pythonnet2.2.2 .
-docker build -t pythonnet/pythonnet:python3.5-mono4.8.0-pythonnet2.2.2 -f Dockerfile-python3.5-mono4.8.0-pythonnet2.2.2 .
+docker build -t pythonnet/pythonnet:python2.7-mono4.8.0-pythonnet2.3 -f Dockerfile-python2.7-mono4.8.0-pythonnet2.3 .
+docker build -t pythonnet/pythonnet:python3.5-mono4.8.0-pythonnet2.3 -f Dockerfile-python3.5-mono4.8.0-pythonnet2.3 .
 
-docker push pythonnet/pythonnet:python2.7-mono4.8.0-pythonnet2.2.2
-docker push pythonnet/pythonnet:python3.5-mono4.8.0-pythonnet2.2.2
+docker push pythonnet/pythonnet:python2.7-mono4.8.0-pythonnet2.3
+docker push pythonnet/pythonnet:python3.5-mono4.8.0-pythonnet2.3
